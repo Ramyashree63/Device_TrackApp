cf3a1973a3328a100e9f0fe1f858e1b2fa3bb6b0
dinesh.r <dinesh.r@gmail.com>
2020-03-18 17:00:48 +0530

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:final_app/ListDetailsScreen/first_screen.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:final_app/UI/login_in.dart';
 
@@ -9,10 +12,39 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: LoginPage()
+    );
+  }
+}
+
+/*SplashScreen.navigate(
+name: "assets/images/splash.flr",
+next: (context) => LoginPage(),
+startAnimation: "1",
+loopAnimation: "1",
+endAnimation: "1",
+until: () => Future.delayed(Duration(seconds: 5)),
+),*/
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState()=> _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: FlareActor(
+          "assets/images/splash.flr",
+          fit: BoxFit.fill,
+          alignment: Alignment.center,
+          animation: "idle",
+        ),
       ),
-      home: LoginPage(),
     );
   }
 }
